e9f2afaa95a80ac97a6da49f29f6fef8489da9c1
0asisCat <khorranebianca@hotmail.com>
2025-01-20 17:56:13 +0800

    put: successfully coded the timer for the game, limited only to minutes

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ const moves = document.getElementById('moves-display');
 const newGameBtn = document.getElementById("new-game");
 const restartBtn = document.getElementById("restart");
 const pauseBtn = document.getElementById("pause");
-let [minutes, seconds] = [0, 0];
+let [milliseconds, seconds, minutes] = [0, 0, 0];
 let int = null;
 let score = 0;
 let move = 0;
@@ -42,13 +42,31 @@ newGameBtn.addEventListener("click", () => {
 
 restartBtn.addEventListener("click", () => {
     clearInterval(int);
-    [minutes, seconds] = [0, 0];
-    timerDisplay.innerHTML = "0 : 00";
+    [milliseconds, seconds, minutes] = [0, 0, 0];
+    timerDisplay.innerHTML = "00 : 00";
 });
 
 pauseBtn.addEventListener("click", () => {
     clearInterval(int);
 });
+
+
+function displayTimer(){
+    milliseconds += 10;
+    if(milliseconds === 1000){
+        milliseconds = 0;
+        seconds++;
+        if(seconds === 60){
+            seconds = 0;
+            minutes++;
+        }
+    }
+
+    let m = minutes < 10 ? "0" + minutes : minutes;
+    let s = seconds < 10 ? "0" + seconds : seconds;
+    
+    timerDisplay.innerHTML = `${m} : ${s}`
+}
 
 const suits = {
     wands: [
