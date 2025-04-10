2c807719380df3b594e0ddcee77c212893f62fb6
0asisCat <khorranebianca@hotmail.com>
2025-04-10 22:02:18 +0800

    post: added isDragging object into false to each cards

--- js/Script.js
+++ js/Script.js
@@ -60,67 +60,67 @@ function pauseDialog(){
 // MAIN DECK
 const suits = {
     wands: [
-        {id:2, src:"asset/minorArc/clubs/club02.png"}, 
-        {id:3, src:"asset/minorArc/clubs/club03.png"}, 
-        {id:4, src:"asset/minorArc/clubs/club04.png"}, 
-        {id:5, src:"asset/minorArc/clubs/club05.png"}, 
-        {id:6, src:"asset/minorArc/clubs/club06.png"}, 
-        {id:7, src:"asset/minorArc/clubs/club07.png"}, 
-        {id:8, src:"asset/minorArc/clubs/club08.png"}, 
-        {id:9, src:"asset/minorArc/clubs/club09.png"}, 
-        {id:10, src:"asset/minorArc/clubs/club10.png"}, 
-        {id:11, src:"asset/minorArc/clubs/club11.png"}, 
-        {id:12, src:"asset/minorArc/clubs/club12.png"}, 
-        {id:13, src:"asset/minorArc/clubs/club13.png"},
-        {id:14, src:"asset/minorArc/clubs/club14.png"}
+        {id:2, src:"asset/minorArc/clubs/club02.png", isDragging: false}, 
+        {id:3, src:"asset/minorArc/clubs/club03.png", isDragging: false}, 
+        {id:4, src:"asset/minorArc/clubs/club04.png", isDragging: false}, 
+        {id:5, src:"asset/minorArc/clubs/club05.png", isDragging: false}, 
+        {id:6, src:"asset/minorArc/clubs/club06.png", isDragging: false}, 
+        {id:7, src:"asset/minorArc/clubs/club07.png", isDragging: false}, 
+        {id:8, src:"asset/minorArc/clubs/club08.png", isDragging: false}, 
+        {id:9, src:"asset/minorArc/clubs/club09.png", isDragging: false}, 
+        {id:10, src:"asset/minorArc/clubs/club10.png", isDragging: false}, 
+        {id:11, src:"asset/minorArc/clubs/club11.png", isDragging: false}, 
+        {id:12, src:"asset/minorArc/clubs/club12.png", isDragging: false}, 
+        {id:13, src:"asset/minorArc/clubs/club13.png", isDragging: false},
+        {id:14, src:"asset/minorArc/clubs/club14.png", isDragging: false}
     ],
     coins: [
-        {id:1, src:"asset/minorArc/diamonds/diamond01.png"},
-        {id:2, src:"asset/minorArc/diamonds/diamond02.png"},
-        {id:3, src:"asset/minorArc/diamonds/diamond03.png"},
-        {id:4, src:"asset/minorArc/diamonds/diamond04.png"},
-        {id:5, src:"asset/minorArc/diamonds/diamond05.png"},
-        {id:6, src:"asset/minorArc/diamonds/diamond06.png"},
-        {id:7, src:"asset/minorArc/diamonds/diamond07.png"},
-        {id:8, src:"asset/minorArc/diamonds/diamond08.png"},
-        {id:9, src:"asset/minorArc/diamonds/diamond09.png"},
-        {id:10, src:"asset/minorArc/diamonds/diamond10.png"},
-        {id:11, src:"asset/minorArc/diamonds/diamond11.png"},
-        {id:12, src:"asset/minorArc/diamonds/diamond12.png"},
-        {id:13, src:"asset/minorArc/diamonds/diamond13.png"},
-        {id:14, src:"asset/minorArc/diamonds/diamond14.png"}
+        {id:1, src:"asset/minorArc/diamonds/diamond01.png", isDragging: false},
+        {id:2, src:"asset/minorArc/diamonds/diamond02.png", isDragging: false},
+        {id:3, src:"asset/minorArc/diamonds/diamond03.png", isDragging: false},
+        {id:4, src:"asset/minorArc/diamonds/diamond04.png", isDragging: false},
+        {id:5, src:"asset/minorArc/diamonds/diamond05.png", isDragging: false},
+        {id:6, src:"asset/minorArc/diamonds/diamond06.png", isDragging: false},
+        {id:7, src:"asset/minorArc/diamonds/diamond07.png", isDragging: false},
+        {id:8, src:"asset/minorArc/diamonds/diamond08.png", isDragging: false},
+        {id:9, src:"asset/minorArc/diamonds/diamond09.png", isDragging: false},
+        {id:10, src:"asset/minorArc/diamonds/diamond10.png", isDragging: false},
+        {id:11, src:"asset/minorArc/diamonds/diamond11.png", isDragging: false},
+        {id:12, src:"asset/minorArc/diamonds/diamond12.png", isDragging: false},
+        {id:13, src:"asset/minorArc/diamonds/diamond13.png", isDragging: false},
+        {id:14, src:"asset/minorArc/diamonds/diamond14.png", isDragging: false}
     ],
     swords: [
-        {id:1, src:"asset/minorArc/spades/spade01.png"},
-        {id:2, src:"asset/minorArc/spades/spade02.png"},
-        {id:3, src:"asset/minorArc/spades/spade03.png"},
-        {id:4, src:"asset/minorArc/spades/spade04.png"},
-        {id:5, src:"asset/minorArc/spades/spade05.png"},
-        {id:6, src:"asset/minorArc/spades/spade06.png"},
-        {id:7, src:"asset/minorArc/spades/spade07.png"},
-        {id:8, src:"asset/minorArc/spades/spade08.png"},
-        {id:9, src:"asset/minorArc/spades/spade09.png"},
-        {id:10, src:"asset/minorArc/spades/spade10.png"},
-        {id:11, src:"asset/minorArc/spades/spade11.png"},
-        {id:12, src:"asset/minorArc/spades/spade12.png"},
-        {id:13, src:"asset/minorArc/spades/spade13.png"},
-        {id:14, src:"asset/minorArc/spades/spade14.png"}
+        {id:1, src:"asset/minorArc/spades/spade01.png", isDragging: false},
+        {id:2, src:"asset/minorArc/spades/spade02.png", isDragging: false},
+        {id:3, src:"asset/minorArc/spades/spade03.png", isDragging: false},
+        {id:4, src:"asset/minorArc/spades/spade04.png", isDragging: false},
+        {id:5, src:"asset/minorArc/spades/spade05.png", isDragging: false},
+        {id:6, src:"asset/minorArc/spades/spade06.png", isDragging: false},
+        {id:7, src:"asset/minorArc/spades/spade07.png", isDragging: false},
+        {id:8, src:"asset/minorArc/spades/spade08.png", isDragging: false},
+        {id:9, src:"asset/minorArc/spades/spade09.png", isDragging: false},
+        {id:10, src:"asset/minorArc/spades/spade10.png", isDragging: false},
+        {id:11, src:"asset/minorArc/spades/spade11.png", isDragging: false},
+        {id:12, src:"asset/minorArc/spades/spade12.png", isDragging: false},
+        {id:13, src:"asset/minorArc/spades/spade13.png", isDragging: false},
+        {id:14, src:"asset/minorArc/spades/spade14.png", isDragging: false}
     ],
     cups: [
-        {id:1, src:"asset/minorArc/hearts/heart01.png"},
-        {id:2, src:"asset/minorArc/hearts/heart02.png"},
-        {id:3, src:"asset/minorArc/hearts/heart03.png"},
-        {id:4, src:"asset/minorArc/hearts/heart04.png"},
-        {id:5, src:"asset/minorArc/hearts/heart05.png"},
-        {id:6, src:"asset/minorArc/hearts/heart06.png"},
-        {id:7, src:"asset/minorArc/hearts/heart07.png"},
-        {id:8, src:"asset/minorArc/hearts/heart08.png"},
-        {id:9, src:"asset/minorArc/hearts/heart09.png"},
-        {id:10, src:"asset/minorArc/hearts/heart10.png"},
-        {id:11, src:"asset/minorArc/hearts/heart11.png"},
-        {id:12, src:"asset/minorArc/hearts/heart12.png"},
-        {id:13, src:"asset/minorArc/hearts/heart13.png"},
-        {id:14, src:"asset/minorArc/hearts/heart14.png"}
+        {id:1, src:"asset/minorArc/hearts/heart01.png", isDragging: false},
+        {id:2, src:"asset/minorArc/hearts/heart02.png", isDragging: false},
+        {id:3, src:"asset/minorArc/hearts/heart03.png", isDragging: false},
+        {id:4, src:"asset/minorArc/hearts/heart04.png", isDragging: false},
+        {id:5, src:"asset/minorArc/hearts/heart05.png", isDragging: false},
+        {id:6, src:"asset/minorArc/hearts/heart06.png", isDragging: false},
+        {id:7, src:"asset/minorArc/hearts/heart07.png", isDragging: false},
+        {id:8, src:"asset/minorArc/hearts/heart08.png", isDragging: false},
+        {id:9, src:"asset/minorArc/hearts/heart09.png", isDragging: false},
+        {id:10, src:"asset/minorArc/hearts/heart10.png", isDragging: false},
+        {id:11, src:"asset/minorArc/hearts/heart11.png", isDragging: false},
+        {id:12, src:"asset/minorArc/hearts/heart12.png", isDragging: false},
+        {id:13, src:"asset/minorArc/hearts/heart13.png", isDragging: false},
+        {id:14, src:"asset/minorArc/hearts/heart14.png", isDragging: false}
     ]
 };
 
@@ -192,7 +192,7 @@ function draw() {
     }
 
     // tableau
-
+    
 }
 
 function createDeck() {
